2eabe6d66a6fc77ca6ba3447f9d6ef1096248c4c
Darien Lopez <83101819+dareslop@users.noreply.github.com>
2022-08-17 21:40:06 -0500

    TrabajoAutonomo5

---
+++ app/src/main/java/com/example/tarea_5/Splash.java
@@ -0,0 +1,29 @@
+package com.example.tarea_5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        TimerTask tiempo = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer time = new Timer ();
+        time.schedule(tiempo, 3000);
+    }
+}
